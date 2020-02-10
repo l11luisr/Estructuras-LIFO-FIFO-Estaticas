9cf516f09d90918c82f6b53072f45f16cd99a70c
José Luis Rodríguez Muñoz <47265230+l11luisr@users.noreply.github.com>
2020-02-10 13:59:57 -0800

    Update P1 Estructuras LIFO y FIFO Estáticas.c

--- P1 Estructuras LIFO y FIFO Estáticas.c
+++ P1 Estructuras LIFO y FIFO Estáticas.c
@@ -1,36 +1,3 @@
-/*	
-											UNIVERSIDAD AUTÓNOMA DE BAJA CALIFORNIA
-										  FACULTAD DE CIENCIAS QUÍMICAS E INGENIERÍA
-											  ALGORITMOS Y ESTRUCTURA DE DATOS
-														 2020 - 1
-					                                    PRÁCTICA #1
-Nombre: Rodríguez Muñoz José Luis
-Práctica: Estructuras LIFO y FIFO Estáticas
-Fecha: 04 / 07 / 2020
-
-Competencia: Aplicar los conceptos básicos de las estructuras LIFO y FIFO.
-
-Actividad:
-
-Escribe un programa que implemente las operaciones básicas y complementarias tanto de colas estáticas como de pilas estáticas.
-El programa debe mostrar el siguiente menú de opciones:
-
-    * Pilas
-        - Push: Introduce elementos a la pila.
-        - Pop: Elimina elementos de la pila (pero antes de eliminarlo lo muestra).
-        - Llena: Regresa verdadero si la pila está llena.
-        - Vacía: Regresa verdadero si la pila está vacía.
-        - Size: Regresa la cantidad de elementos que contiene la pila.
-        - Mostrar: Muestra los elementos que contiene la pila.
-
-    * Colas
-        - Insertar: Introduce elementos a la cola.
-        - Remover: Elimina elementos de la cola.
-        - Llena: Regresa verdadero si la cola está llena.
-        - Vacía: Regresa verdadero si la cola está vacía.
-        - Mostrar: Muestra los elementos que contiene la cola.
-*/
-
 #include <stdio.h> // Librería principal
 #include <conio.h> // Librería para el uso de getch
 #define MAX 5 // Valor máximo para la pila y cola
